e9305271711fb4c4fdf7004d36c71f4b0e50f6bb
Adeniltonarcanjo <adeniltonmorais@gmail.com>
2022-03-06 09:56:56 -0300

    Final project GFT

---
+++ src/main/java/one/digitalinovationgft/labpadroesprojetospring/model/ClienteRepository.java
@@ -0,0 +1,10 @@
+package one.digitalinovationgft.labpadroesprojetospring.model;
+
+import one.digitalinovationgft.labpadroesprojetospring.model.Cliente;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
